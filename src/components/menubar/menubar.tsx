74c05f2829220fcf039dd7d7872da87e7147e3b5
ngdream <nguewofoss@gmail.com>
2023-02-04 21:51:08 +0100

    migrating to type  script nd adding somes changes

--- src/components/menubar/menubar.tsx
+++ src/components/menubar/menubar.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 class Menubar extends React.Component {
     render() {
         return (
-            <div class="menu-bar">
-            <div class="menu">
+            <div className="menu-bar">
+            <div className="menu">
                 <a href="">file</a>
                 <a href="">item</a>
                 <a href="">view</a>
